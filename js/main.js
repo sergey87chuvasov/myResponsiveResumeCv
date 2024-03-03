92e0bc5d361766e6eb66a17e228da978723f6fd2
Sergey87 <serge.techno@gmail.com>
2024-03-04 02:16:04 +0300

    fixed project

--- js/main.js
+++ js/main.js
@@ -98,10 +98,10 @@ function removeScale() {
 
 let opt = {
   margin: 0,
-  filename: 'myResume1.pdf',
+  filename: 'myResume.pdf',
   image: { type: 'jpeg', quality: 0.98 },
   html2canvas: { scale: 4 },
-  jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+  jsPDF: { format: 'a4', orientation: 'portrait' },
 };
 
 let areaCv = document.getElementById('area-cv');
